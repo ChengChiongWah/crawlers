84276f3df640225f7c211c83881e2129c18ecfb5
ChengChiongWah <chengchiongwah@gmail.com>
2017-03-22 12:01:26 +0800

    the 2

--- 51job/51job_crawler.py
+++ 51job/51job_crawler.py
@@ -2,31 +2,48 @@ import requests, os
 from lxml import html
 
 
+def cache_url(url, pagenumber):
+    """把页面下载下来
+    """
+    filename = str(pagenumber) + ".html"
+    path = os.path.join("cache" + filename)
+    if os.path.exists(path):
+        with open(path, "rb") as f:
+            return f.read()
+    else:
+        r = requests.get(url)
+        with open(path, "wb") as f:
+            f.write(r.content)
+
+
+
 def main():
-    url = "http://my.51job.com/my/history/My_AppHistory.php?pageno={}&total=&rand=840900682".format(1)
-    print(url)
-    cookies_content = ("guid=14900818708430220023; 51job=cuid%3D32340218%26%7C%26cusername%3Dsguzch%25"
-    "40163.com%26%7C%26cpassword%3D%26%7C%26cname%3D%25D6%25A3%25B3%25A3%25BB%25AA%26%7C%26cemail%3D"
-    "sguzch%2540163.com%26%7C%26cemailstatus%3D3%26%7C%26cnickname%3Dsguzch%26%7C%26ccry%3D.0IIHvuTZ6"
-    "r6I%26%7C%26cconfirmkey%3Dsg7pJGOaUQhus%26%7C%26cresumeids%3D.0KFcKZQ4tO76%257C.0ftGf7lIbr.I%25"
-    "7C.0tZoltd6muwQ%257C%26%7C%26cautologin%3D1%26%7C%26cenglish%3D0%26%7C%26sex%3D0%26%7C%26cnameke"
-    "y%3DsgvfYAehpiU5o%26%7C%26to%3DWmFaMwJjCzlUNQBuUDRRagB%252FVyUGZ1p6Ui4FNAxuVhYNNABtBGUHL1dlDj0LM"
-    "1coADIGMlNnUjZSYgZlDDEAOFpqWjk%253D%26%7C%26%3D; ps=us%3DDDVUO1QoADdSNwpjVi1WZFdgBjNSegdkVmhUegky"
-    " DzsBMAVuBGZWaFMzCm4FaFxnADRUbQY5UXgHR1ZWWzNVYgxg%26%7C%26needv%3D0; nolife=fromdomain%3Dwww; sli"
-    "fe=resumeguide%3D1%26%7C%26")
+    url = "http://my.51job.com/my/history/My_AppHistory.php?pageno={}&total=&rand=840900682"
+    url_root = url.format(1)
+    cookies_content = ("guid=14901546665369820047; ps=us%3DCzJVOgR4ATYCZwxlVS5SYAUyCj8BKQdkXGJccgox"
+                       "UWUMPQVuBWYDMABlAWtSPwM4AToDN1BuVn8HWQEZWzVSZAt2%26%7C%26needv%3D0; 51job=cui"
+                       "d%3D32340218%26%7C%26cusername%3Dsguzch%2540163.com%26%7C%26cpassword%3D%26%7C"
+                       "%26cname%3D%25D6%25A3%25B3%25A3%25BB%25AA%26%7C%26cemail%3Dsguzch%2540163.com%26"
+                       "%7C%26cemailstatus%3D3%26%7C%26cnickname%3Dsguzch%26%7C%26ccry%3D.0IIHvuTZ6r6I%2"
+                       "6%7C%26cconfirmkey%3Dsg7pJGOaUQhus%26%7C%26cresumeids%3D.0KFcKZQ4tO76%257C.0ftGf7"
+                       "lIbr.I%257C.0tZoltd6muwQ%257C%26%7C%26cautologin%3D1%26%7C%26cenglish%3D0%26%7C%2"
+                       "6sex%3D0%26%7C%26cnamekey%3DsgvfYAehpiU5o%26%7C%26to%3DXGdUPQRlBDZUNV0zUTVUb1AvB"
+                       "nRaOwcnUi5XZgBiDk4NNAJvUDEDK1NhXG9RaQZ5BTcBNVtvUzcFNVE6WGIEMlxsVDc%253D%26%7C%26")
     headers = {
         "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0",
         "Cookie":cookies_content,
     }
-    print(cookies_content)
     path = os.path.join("cached", "1.html")
-    page = requests.get(url, headers=headers).text
+    page = requests.get(url_root, headers=headers).text
     root_page = html.fromstring(page)
-    page_number = root_page.xpath("//strong[@class='orareg']")
-    print(len(page_number))
-    # r = requests.get(url, headers=headers)
-    # with open(path, "wb") as f:
-    #     f.write(r.content)
+    xpath_page_number = root_page.xpath("//strong[@class='orareg']")
+    page_number = xpath_page_number[0].text.rsplit("/", 1)[1]
+    page_number = int(page_number)
+    number = 1
+    print(url.format(number), page_number)
+    while number <= page_number:
+        cache_url(url.format(number), number)
+        number =+ 1
 
 
 if __name__ == "__main__":
